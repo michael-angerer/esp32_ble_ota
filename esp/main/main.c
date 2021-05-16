35de3ffcd9aa71489d71c470182c88b8f0c5178a
Michael Angerer <angerer.mic@gmail.com>
2021-05-16 13:43:35 +0200

    enable app rollback and detect the running partition

--- esp/main/main.c
+++ esp/main/main.c
@@ -6,9 +6,47 @@
 
 #define LOG_TAG_MAIN "main"
 
+bool run_diagnostics() {
+  // do some diagnostics
+  return true;
+}
+
 void app_main(void) {
-  const esp_app_desc_t *app_desc = esp_ota_get_app_description();
-  ESP_LOGI(LOG_TAG_MAIN, "App version: %s", app_desc->version);
+  // check which partition is running
+  const esp_partition_t *partition = esp_ota_get_running_partition();
+
+  switch (partition->address) {
+    case 0x00010000:
+      ESP_LOGI(LOG_TAG_MAIN, "Running partition: factory");
+      break;
+    case 0x00110000:
+      ESP_LOGI(LOG_TAG_MAIN, "Running partition: ota_0");
+      break;
+    case 0x00210000:
+      ESP_LOGI(LOG_TAG_MAIN, "Running partition: ota_1");
+      break;
+
+    default:
+      ESP_LOGE(LOG_TAG_MAIN, "Running partition: unknown");
+      break;
+  }
+
+  // check if an OTA has been done, if so run diagnostics
+  esp_ota_img_states_t ota_state;
+  if (esp_ota_get_state_partition(partition, &ota_state) == ESP_OK) {
+    if (ota_state == ESP_OTA_IMG_PENDING_VERIFY) {
+      ESP_LOGI(LOG_TAG_MAIN, "An OTA update has been detected.");
+      if (run_diagnostics()) {
+        ESP_LOGI(LOG_TAG_MAIN,
+                 "Diagnostics completed successfully! Continuing execution.");
+        esp_ota_mark_app_valid_cancel_rollback();
+      } else {
+        ESP_LOGE(LOG_TAG_MAIN,
+                 "Diagnostics failed! Start rollback to the previous version.");
+        esp_ota_mark_app_invalid_rollback_and_reboot();
+      }
+    }
+  }
 
   // Initialize NVS
   esp_err_t ret = nvs_flash_init();
